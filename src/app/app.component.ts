17cb6df21ab98ba7b69d98641d52f1b7ca70ad41
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-14 15:23:58 +0100

    default values mapped from typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   
   signUpForm: FormGroup;
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      
+      username: new FormControl('Afolabi Oluseun Opakunle'),
+      email: new FormControl('afolabi@gmail.com'),
+      gender: new FormControl('male')
     })
   }
 }
